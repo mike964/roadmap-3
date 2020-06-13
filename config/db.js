9fe21be36e67fcc289ff92b5ab58949dc6e267e1
Moslm Iraqi <mike7olwo@gmail.com>
2020-06-13 12:28:13 +0300

    updated

--- config/db.js
+++ config/db.js
@@ -2,18 +2,34 @@ const mongoose = require( 'mongoose' )
 require( 'dotenv' ).config()
 
 // const mongoURI = process.env.MONGO_URI
-const mongoURI = "mongodb+srv://mike:mike1234@cluster0-gpzsv.mongodb.net/roadmap-db?retryWrites=true&w=majority"
 // const mongoURI = "mongodb://localhost/moslm-roadmap"
+
+let mongoURI
+let connectionMsg
+
+console.log( "process.env.NODE_ENV: " + process.env.NODE_ENV )
+
+if ( process.env.LOCAL_DB === "yes" ) {
+  // mongoURI = "mongodb://localhost/moslm-roadmap"
+  mongoURI = process.env.LOCAL_DB_URI
+  connectionMsg = "Local DB connected ..."
+} else {
+  // mongoURI = "mongodb+srv://mike:mike1234@cluster0-gpzsv.mongodb.net/roadmap-db?retryWrites=true&w=majority"
+  mongoURI = process.env.ATLAS_URI
+  connectionMsg = "ATLAS DB connected ..."
+}
+
+
 
 
 const connectDB = () => {
-  mongoose.connect( uri, {
+  mongoose.connect( mongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
   } )
-    .then( () => console.log( 'Database Connected' ) )
+    .then( () => console.log( connectionMsg ) )
     .catch( err => console.log( err ) )
 }
 
