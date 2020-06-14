b26d62ce19e116fdca68cd3255e75de619bddcc0
Moslm Iraqi <mike7olwo@gmail.com>
2020-06-14 23:50:50 +0300

    password removed from getMe

--- controllers/auth.cont.js
+++ controllers/auth.cont.js
@@ -73,7 +73,7 @@ exports.logout = asyncHandler( async ( req, res, next ) => {
 // @route     POST /api/v1/auth/me
 // @access    Private
 exports.getMe = asyncHandler( async ( req, res, next ) => {
-  const user = await User.findById( req.user.id )
+  const user = await User.findById( req.user.id ).select( '-password' )
 
   res.status( 200 ).json( {
     success: true,
